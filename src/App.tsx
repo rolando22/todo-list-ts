3d81d5e5ca5cc739002303ff1848768aca066652
rolando22 <rolando2207@gmail.com>
2023-07-10 12:45:54 -0300

    [ADD] extrayendo la lógica de las tareas en un customHook

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,23 @@ import { useState } from 'react';
 
 import { Footer, Header, Todos } from './components/';
 
-import type {  FilterValue, Todo as TodoType, TodoTypeId, TodoTypeTitle } from './types';
+import type {  FilterValue } from './types';
 import { TODO_FILTERS } from './const';
+import { useTodos } from './hooks/useTodos';
 
-const mockTodos = [
-    {
-        id: '0', 
-        title: 'Title 1',
-        completed: true,
-    },
-    {
-        id: '1', 
-        title: 'Title 2',
-        completed: false,
-    },
-    {
-        id: '2', 
-        title: 'Title 3',
-        completed: false,
-    },
-];
+
 
 export const App = (): JSX.Element => {
-    const [todos, setTodos] = useState(mockTodos);
     const [filter, setFilter] = useState<FilterValue>(TODO_FILTERS.ALL);
-
-    const onRemoveTodo = ({ id }: TodoTypeId) => {
-        const newTodos = todos.filter(todo => todo.id !== id);
-        setTodos(newTodos);
-    };
-
-    const onToggleCompleteTodo = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
-        const newTodos = todos.map(todo => {
-            if (todo.id === id) return { ...todo, completed };
-            return todo;
-        });
-        setTodos(newTodos);
-    };
-
-    const activeCount = todos.filter(todo => !todo.completed).length;
-    const completedCount = todos.length - activeCount;
+    const { 
+        todos,
+        activeCount,
+        completedCount,
+        onRemoveTodo,
+        onToggleCompleteTodo,
+        onClearCompleted,
+        onAddTodo 
+    } = useTodos();
 
     const onFilterChange = (filter: FilterValue): void => setFilter(filter);
 
@@ -50,21 +27,6 @@ export const App = (): JSX.Element => {
         if (filter === TODO_FILTERS.COMPLETED) return todo.completed;
         return todo;
     });
-
-    const onClearCompleted = (): void => {
-        const newTodos = todos.filter(todo => !todo.completed);
-        setTodos(newTodos);
-    };
-
-    const onAddTodo = ({ title }: TodoTypeTitle): void => {
-        const newTodo = {
-            id: crypto.randomUUID(),
-            title,
-            completed: false,
-        };
-        const newTodos = [...todos, newTodo];
-        setTodos(newTodos);
-    };
 
     return (
         <main className='todoapp'>
---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import type { TodoList, Todo as TodoType, TodoTypeId, TodoTypeTitle } from "../types";
+
+const mockTodos = [
+    {
+        id: '0', 
+        title: 'Title 1',
+        completed: true,
+    },
+    {
+        id: '1', 
+        title: 'Title 2',
+        completed: false,
+    },
+    {
+        id: '2', 
+        title: 'Title 3',
+        completed: false,
+    },
+];
+
+export const useTodos = (): {
+    todos: TodoList,
+    activeCount: number,
+    completedCount: number,
+    onRemoveTodo: ({ id }: TodoTypeId) => void,
+    onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void,
+    onClearCompleted: () => void,
+    onAddTodo: ({ title }: TodoTypeTitle) => void,
+} => {
+    const [todos, setTodos] = useState(mockTodos);
+
+    const activeCount = todos.filter(todo => !todo.completed).length;
+    const completedCount = todos.length - activeCount;
+
+    const onRemoveTodo = ({ id }: TodoTypeId) => {
+        const newTodos = todos.filter(todo => todo.id !== id);
+        setTodos(newTodos);
+    };
+
+    const onToggleCompleteTodo = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
+        const newTodos = todos.map(todo => {
+            if (todo.id === id) return { ...todo, completed };
+            return todo;
+        });
+        setTodos(newTodos);
+    };
+
+    const onClearCompleted = (): void => {
+        const newTodos = todos.filter(todo => !todo.completed);
+        setTodos(newTodos);
+    };
+
+    const onAddTodo = ({ title }: TodoTypeTitle): void => {
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title,
+            completed: false,
+        };
+        const newTodos = [...todos, newTodo];
+        setTodos(newTodos);
+    };
+
+    return {
+        todos,
+        activeCount,
+        completedCount,
+        onRemoveTodo,
+        onToggleCompleteTodo,
+        onClearCompleted,
+        onAddTodo,
+    };
+};
